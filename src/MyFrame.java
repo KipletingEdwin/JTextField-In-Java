7a7b306c897e05fe4d1bb351815a8ef5eac098e8
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-11 12:09:24 +0300

    add an instance of MyFrame

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -8,11 +8,11 @@ public class MyFrame extends JFrame implements ActionListener {
     MyFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
+
+
         this.pack();
         this.setVisible(true);
     }
-
-
 
 
     @Override
